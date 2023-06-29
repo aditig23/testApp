2bf93a8a38a0e3c1922b50d7f1141da605efedcb
Aditi Gupta <guptaditi232008@gmail.com>
2023-06-29 11:07:43 -0700

    changed text

--- mentalHealth.swift
+++ mentalHealth.swift
@@ -19,7 +19,7 @@ struct mentalHealth: View {
                     .fontWeight(.bold)
                     .foregroundColor(Color(hue: 0.344, saturation: 0.946, brightness: 0.521))
                 
-                Text("Mental health is crucial, and raising awareness about anxiety and depression is vital. Going through mental health problems is very common, with around 1 in 5 adults suffering a mental illness. Coping mechanisms like journaling offer an effective way to manage mental well-being. Journaling allows for self-expression, self-reflection, and tracking emotional patterns. By incorporating journaling into daily routines, individuals can enhance resilience and gain valuable insights into their mental health.")
+                Text("Mental health is crucial, and raising awareness about anxiety and depression is vital. Going through mental health issues is very common, with around 1 in 5 adults suffering a mental illness. Coping mechanisms like journaling offer an effective way to manage mental well-being. Journaling allows for self-expression, self-reflection, and tracking emotional patterns. By incorporating journaling into daily routines, individuals can enhance resilience and gain valuable insights into their mental health.")
                     .fontWeight(.semibold)
                     .foregroundColor(Color(hue: 0.396, saturation: 0.945, brightness: 0.381))
                     .multilineTextAlignment(.center)
